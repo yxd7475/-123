65924ccdbbf34dc2b7c7550102ccecc3c63c9916
yxd7475 <1685349214@qq.com>
2026-03-17 18:13:48 +0800

    修改穿透工具域名随机变化连接不上问题

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,14 +13,15 @@ export default defineConfig(({ mode }) => {
       }
     },
     server: {
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: env.VITE_DEV_API_TARGET || 'http://localhost:5000',
-          changeOrigin: true
+  host: '0.0.0.0',
+  port: 3000,
+  allowedHosts: true,
+  proxy: {
+    '/api': {
+      target: env.VITE_DEV_API_TARGET || 'http://127.0.0.1:5000',
+      changeOrigin: true
         }
       }
     }
   }
-}
-)
+})
